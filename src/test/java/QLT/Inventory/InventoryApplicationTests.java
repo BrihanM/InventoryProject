ebf2eb7db4815c391ceb58c9ddd3d846060d7b0c
Brayan M <brihanm.work@gmail.com>
2024-04-10 00:41:39 -0500

    Principal class

---
+++ src/test/java/QLT/Inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package QLT.Inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
